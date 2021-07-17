ecfa94cbfadae7bde02758415921c9ff68261bb7
dvega23 <87511013+dvega23@users.noreply.github.com>
2021-07-17 14:32:22 -0500

    Update Usuario.java

--- src/Usuario/Usuario.java
+++ src/Usuario/Usuario.java
@@ -1,32 +1,33 @@
 package Usuario;
 
 
-import Porpiedades.PropiedadesEvaluadas;
-import Sensor.Sensor;
+
 import Notificacion.Notificacion;
-import Notificacion.NotificacionPropiedad;
-import Notificacion.NotificacionSensor;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.Set;
 
 public class Usuario {
     private String nombreUsuario;
-    private String contraseña;   
-    private String propiedadEvaluar = "co";
-    private String rangoPeligro;
-    private String rangoModerado;
+    private String contraseña;
+    private HashMap<String,String> propiedades;   
     private ArrayList<String> sensoresIds = new ArrayList<>();
     private ArrayList<Notificacion> notificaciones = new ArrayList<>();   
-    
 
-    public Usuario(String nombreUsuario, String contraseña,String rangoPeligro,String rangoModerado) {        
+    public Usuario(String nombreUsuario, String contraseña) {
+        this.propiedades = new HashMap<>();
         this.nombreUsuario = nombreUsuario;
-        this.contraseña = contraseña;  
-        this.rangoPeligro = rangoPeligro;
-        this.rangoModerado = rangoModerado;
+        this.contraseña = contraseña;
     }    
-    
-    public Usuario(){}
+    public Usuario(String nombreUsuario, String contraseña,String propiedad,String rangoPeligro,String rangoModerado) {        
+        this.propiedades = new HashMap<>();
+        this.propiedades.put(propiedad, rangoPeligro+"/"+rangoModerado);
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña; 
+    }   
+    public Usuario(){        
+    }        
     public String getNombreUsuario() {
         return nombreUsuario;
     }
@@ -39,44 +40,53 @@ public class Usuario {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     } 
-    public String getPropiedadEvaluar() {
-        return propiedadEvaluar;
+    public HashMap<String,String> getPropiedades() {
+        return this.propiedades;
     }
     public ArrayList<String> getSensoresIds() {
         return sensoresIds;
     }
     public ArrayList<Notificacion> getNotificaciones() {
         return notificaciones;
-    }   
-    public String getRangoPeligro() {
-        return rangoPeligro;
-    }
-    public void setRangoPeligro(String rangoPeligro) {
-        this.rangoPeligro = rangoPeligro;
-    }
-    public String getRangoModerado() {
-        return rangoModerado;
-    }
-    public void setRangoModerado(String rangoModerado) {
-        this.rangoModerado = rangoModerado;
-    }
-    
-    
+    }      
     @Override
-    public boolean equals(Object obj) {        
-        if(obj == null){
-        return false;
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
         }
         final Usuario other = (Usuario) obj;
-        if (!Objects.equals(this.nombreUsuario, other.nombreUsuario) && !Objects.equals(this.contraseña, other.contraseña)) {
+        if (!Objects.equals(this.nombreUsuario, other.nombreUsuario)) {
             return false;
-        }       
+        }
+        if (!Objects.equals(this.contraseña, other.contraseña)) {
+            return false;
+        }
         return true;
-    }   
-
+    }  
     @Override
-    public String toString() {
-        return nombreUsuario;
+    public String toString(){
+        String propiedades = "";
+        String sensores = "";
+        String valores = "";
+        for(Object p:this.propiedades.keySet()){
+            propiedades +=p+";";
+        }
+        for(Object v:this.propiedades.values()){
+            valores+=v+";";
+        }
+        for(String s:this.sensoresIds){           
+            sensores+=s+";";  
+        }        
+        return "Usuario:"+this.getNombreUsuario()+"-"+"Contraseña:"
+                        +this.getContraseña()+"-Propiedad:"+propiedades+"-"
+                +valores+"-"+sensores;
     }
-    
 }
+    
+    
