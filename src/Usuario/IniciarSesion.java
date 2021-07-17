27991a1f4e00125fe6f2c227898a0454c176cc69
dvega23 <87511013+dvega23@users.noreply.github.com>
2021-07-17 14:33:00 -0500

    Update IniciarSesion.java

--- src/Usuario/IniciarSesion.java
+++ src/Usuario/IniciarSesion.java
@@ -1,5 +1,4 @@
 package Usuario;
-import Usuario.Usuario;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -11,37 +10,66 @@ public class IniciarSesion {
     public static ArrayList<Usuario> listaUsuarios = new ArrayList<>();    
    
     public static void leerListaUsuarios(){
-       Usuario u;
+       Usuario u=null;
         try (BufferedReader lector = new BufferedReader(new FileReader("ListaUsuarios.txt"))){             
-            String line = "";                        
-            while((line = lector.readLine())!= null){                
-                String [] sep = line.split("-");
+            String line = "";            
+            while((line = lector.readLine()) != null){                
+                String [] sep = line.split("-");                
                 String[] user = sep[0].split(":");
                 String[] pass = sep[1].split(":");
-                String[]rangoValores = sep[2].split("/");
-                u = new Usuario(user[1],pass[1],rangoValores[0],rangoValores[1]);
-                listaUsuarios.add(u);
-            }            
-        } 
+                String[] valores = sep[3].split(";");//[valor1,valor2....]
+                String[] p = sep[2].split(":");
+                String[] propiedades = p[1].split(";");
+                String[] sensores=null;
+                try{
+                sensores = sep[4].split(";");}                
+                catch(ArrayIndexOutOfBoundsException ex){
+                    //JOptionPane.showMessageDialog(null, "NO TIENE SENSORES ENROLADOS");
+                }
+                u = new Usuario(user[1],pass[1]);
+                for(int i=0;i<propiedades.length;i++){
+                    String[] rangoValores = valores[i].split("/");
+                    u.getPropiedades().put(propiedades[i],rangoValores[0]+"/"+rangoValores[1]);                    
+                    }                    
+                        try{
+                        for(String sensor:sensores){                                                        
+                            u.getSensoresIds().add(sensor);
+                                }
+                            }                        
+                        catch(NullPointerException ex){
+                            //JOptionPane.showMessageDialog(null, "NO HAY SENSORES");
+                            }
+                        listaUsuarios.add(u);
+                        }                    
+                    }          
     catch (FileNotFoundException ex) {
             System.err.println("No existe el archivo: "+ex);
         } 
     catch (IOException ex) {
             System.err.println("Error lectura del archivo: "+ex);
-        }  
-    }   
-    public static Usuario login(String nombre,String pass){               
-        for(Usuario user:listaUsuarios){
-            if (nombre.equals(user.getNombreUsuario())&& pass.equals(user.getContraseña())){
+        }
+    } 
+    public static Usuario login(String nombre,String pass){   
+        Usuario u = new Usuario(nombre,pass);        
+        if(pass.equals("")){
+           JOptionPane.showMessageDialog(null,"INGRESE UNA CONTRASEÑA VALIDA"); 
+            JOptionPane.showMessageDialog(null,"INICIO DE SESION FALLIDO"); 
+            return null;
+        }
+        boolean existe = false;
+        for(Usuario user:listaUsuarios){               
+            if (user.equals(u)){
+               existe = true;
                JOptionPane.showMessageDialog(null,"INICIO DE SESION CORRECTO");
                return user;
                 }
-            else{                  
-               JOptionPane.showMessageDialog(null,"Usuario no registrado"); 
-               JOptionPane.showMessageDialog(null,"INICIO DE SESION FALLIDO");
-               return null;
-           }
-        }
-        return null;
-    }
-}   
+            }
+        if(!existe){
+            JOptionPane.showMessageDialog(null,"Usuario no registrado"); 
+            JOptionPane.showMessageDialog(null,"INICIO DE SESION FALLIDO");
+            return null;
+            }
+    return null;   
+        }    
+}
+   
